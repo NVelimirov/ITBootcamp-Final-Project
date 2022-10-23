9436474d97c4a962d2bf48a996779d986ef617d7
NVelimirov <112095742+NVelimirov@users.noreply.github.com>
2022-10-23 02:43:51 +0200

    updated TestConfluxHomePage

--- src/helpers/URLs.java
+++ src/helpers/URLs.java
@@ -2,4 +2,5 @@ package helpers;
 
 public class URLs {
     public static final String HOME_PAGE = "https://conflux.rs/";
+    public static final String GOOGLE_SEARCH = "https://www.google.com/";
 }
--- src/tests/TestConfluxHomePage.java
+++ src/tests/TestConfluxHomePage.java
@@ -1,80 +1,62 @@
 package tests;
 
-import org.openqa.selenium.interactions.Actions;
+import base.BaseTest;
 import org.testng.annotations.*;
 import pages.ConfluxHomePage;
 import pages.GoogleSearch;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import helpers.Page_TitlesTextsAndMessages;
 
 
-import java.time.Duration;
+import static helpers.URLs.GOOGLE_SEARCH;
+import static helpers.URLs.HOME_PAGE;
 
-public class TestConfluxHomePage {
+public class TestConfluxHomePage extends BaseTest {
 //region Fields
-    private WebDriver webDriver;
-
-    private Actions actions;
-
     private GoogleSearch googleSearch;
-
     private ConfluxHomePage confluxHomePage;
 
 //endregion
 //region BeforeTest
-    @BeforeTest
+    @BeforeMethod
     public void config(){
-        System.setProperty("webdriver.chrome.driver", utils.PropertiesReader.getInstance().getValue("WEBDRIVER.CHROMEDRIVER"));
-        webDriver = new ChromeDriver();
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        actions = new Actions(webDriver);
         googleSearch = new GoogleSearch(webDriver);
         confluxHomePage = new ConfluxHomePage(webDriver,actions);
-        webDriver.get("https://www.google.com/");
         webDriver.manage().window().maximize();
+        webDriver.get(HOME_PAGE);
     }
 //endregion
 //region Test
-    @Test
+    @Test (priority = 10)
     public void navigateToConfluxPage(){
+        webDriver.navigate().to(GOOGLE_SEARCH);
         googleSearch.googleSearchInput();
         googleSearch.navigateToConfluxPage();
         Assert.assertEquals(confluxHomePage.checkIfOnHomePage(), Page_TitlesTextsAndMessages.HOME_PAGE_TEXT.getAbsolutePath(), "Text should match");
     }
-    @Test (dependsOnMethods = "navigateToConfluxPage")
+    @Test (priority = 20)
     public void testFacebookLink(){
         confluxHomePage.toFacebookPage();
         Assert.assertEquals(confluxHomePage.checkIfFacebookPageHasOpened(), Page_TitlesTextsAndMessages.FACEBOOK_PAGE_TITLE.getAbsolutePath(), "Titles should match");
-        webDriver.switchTo().window(webDriver.getWindowHandle());
     }
-    @Test (dependsOnMethods = "navigateToConfluxPage")
+    @Test (priority = 30)
     public void testInstagramLink(){
         confluxHomePage.toInstagramPage();
         Assert.assertEquals(confluxHomePage.checkIfInstagramPageHasOpened(), Page_TitlesTextsAndMessages.FACEBOOK_PAGE_TITLE.getAbsolutePath(), "Titles should match");
-        webDriver.switchTo().window(webDriver.getWindowHandle());
     }
-    @Test(dependsOnMethods = "navigateToConfluxPage")
+    @Test (priority = 40)
     public void changePageLanguageToSerbian(){
         confluxHomePage.changeLanguageToSerbian();
         Assert.assertEquals(confluxHomePage.checkIfLanguageHasChangedToSerbian(), Page_TitlesTextsAndMessages.HOME_PAGE_LANGUAGE_SERBIAN.getAbsolutePath(), "It should say Srpski");
     }
-
-
-    //hocemo da se ovo izvrsi samo posle odredjenih metoda
-    @AfterMethod(dependsOnMethods = {"testInstagramLink", "testFacebookLink"})
-    public void switchTo() {
-        webDriver.switchTo().window(webDriver.getWindowHandle());
-    }
-    @Test(dependsOnMethods = "changePageLanguageToSerbian")
+    @Test (priority = 50)
     public void revertToEnglish(){
         confluxHomePage.revertLanguageToEnglish();
         Assert.assertEquals(confluxHomePage.checkIfLanguageHasChangedToEnglish(), Page_TitlesTextsAndMessages.HOME_PAGE_LANGUAGE_ENGLISH.getAbsolutePath(), "It should say English");
     }
 //endregion
-    @AfterTest
-    public void closeWebDriver(){
-        webDriver.quit();
+    @AfterMethod(dependsOnGroups = {"testFacebookLink", "testInstagramLink"})
+    public void switchTo() {
+        webDriver.switchTo().window(webDriver.getWindowHandle());
     }
 }
